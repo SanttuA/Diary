04486ec242038f0f5b6b97f6b5fb82cbd5976e4b
SanttuA <santtualatalo@gmail.com>
2017-04-17 11:01:39 +0300

    removed database class and removed date textfield from main activity

--- app/src/main/java/com/example/santtu/diary/MainActivity.java
+++ app/src/main/java/com/example/santtu/diary/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.santtu.diary;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.format.DateFormat;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
+
+    Date date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +27,18 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //Today's date
+        date = new Date();
+        String dateString = DateFormat.getDateFormat(getApplicationContext()).format(date);
+        String titleString = getTitle() + " - " + dateString;
+        setTitle(titleString);
+
+        //get the database data for today's date if one exists
+
+
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -40,5 +60,34 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+
+        //save the data in diary edit text
+    }
+
+    public void GoToPreviousDiaryEntry()
+    {
+        //if previous exists, load previous diary entry from database
+    }
+
+    public void GoToNextDiaryEntry()
+    {
+        //if next exists, load next diary entry from database
+    }
+
+
+    private class AccessDatabaseTask extends AsyncTask<String, Void, String>
+    {
+        @Override
+        protected String doInBackground(String... params) {
+
+
+            return null;
+        }
     }
 }
